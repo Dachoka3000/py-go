b55fcf4db8c6e5457ea7366ae228a4c856a299f3
Machoka Daisy <machokadaisy25@gmail.com>
2020-11-01 18:07:47 +0300

    register all models in admin

--- hood/admin.py
+++ hood/admin.py
@@ -1,5 +1,10 @@
 from django.contrib import admin
-from .models import Hood
+from .models import Hood,Business,Profile,Post,Contact
 
 # Register your models here.
 admin.site.register(Hood)
+admin.site.register(Business)
+admin.site.register(Profile)
+admin.site.register(Post)
+admin.site.register(Contact)
+
