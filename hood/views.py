9047f935b5e0129bc7ae386be2e0e6dbab5cdd96
Machoka Daisy <machokadaisy25@gmail.com>
2020-11-02 04:09:00 +0300

    add view function to save new businesses to database

--- hood/views.py
+++ hood/views.py
@@ -2,7 +2,8 @@ from django.shortcuts import render,redirect
 from django.http import HttpResponse
 from django.contrib.auth.decorators import login_required,permission_required
 from django.contrib.auth.models import User
-from .forms import AddProfileForm
+from .forms import AddProfileForm,AddBusinessForm,AddPostForm
+from .models import Profile,Post,Business,Hood,Contact
 from cloudinary.forms import cl_init_js_callbacks      
 
 
@@ -14,7 +15,7 @@ def homepage(request):
 def about(request):
     return render(request,'hood/about.html')
 
-@login_required(login_url='login')
+@login_required(login_url='/accounts/login/')
 def addprofile(request):
     current_user=request.user
 
@@ -28,6 +29,31 @@ def addprofile(request):
     else:
         form=AddProfileForm()
     return render(request, 'hood/addprofile.html',{"form":form})
+
+@login_required(login_url='/accounts/login/')
+def profile(request):
+    current_user = request.user
+    profile = Profile.objects.get(user=current_user)
+    businesses = Business.objects.filter(owner=current_user)
+    posts = Post.objects.filter(poster=current_user)
+    return render(request,'hood/profile.html',{"current_user":current_user,"profile":profile,"businesses":businesses,"posts":posts})
+
+@login_required(login_url='/accounts/login/')
+def addbusiness(request):
+    current_user=request.user
+
+    if request.method=="POST":
+        form=AddBusinessForm(request.POST)
+        if form.is_valid():
+            new_business=form.save(commit=False)
+            new_business.owner=current_user
+            new_business.save()
+            return redirect('business')
+    else:
+        form=AddBusinessForm()
+    return render(request,'hood/addbusiness.html',{"current_user":current_user,"form":form})
+
+
             
 
 # def addprofile(request):
