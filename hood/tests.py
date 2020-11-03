1477d0bcd246ca013967f90d4d9636a7df9670d7
Machoka Daisy <machokadaisy25@gmail.com>
2020-11-03 12:43:40 +0300

    add tests for contact model

--- hood/tests.py
+++ hood/tests.py
@@ -16,15 +16,37 @@ class HoodTestCase(TestCase):
         occupants=678
         adder=self.daisy
 
-        self.hoodie=Hood.create_neighbourhood(self,name,location,occupants,adder)
+        self.hoodie=Hood.create_neighbourhood(name=name,location=location,occupants=occupants,adder=adder)
         self.assertTrue(isinstance(self.hoodie,Hood))
 
     
 
-# class ContactTestCase(TestCase):
-#     '''
-#     TestCase that runs test for contact model objects
-#     '''
+class ContactTestCase(TestCase):
+    '''
+    TestCase that runs test for contact model objects
+    '''
+    def setUp(self):
+        User = get_user_model()
+        self.daisy = User(username = "daisy", email="daisy@email.com", password = "password")
+        self.daisy.save()
+        self.hood = Hood(name="local",location="area",occupants=5,adder=self.daisy)
+        self.hood.save()
+        self.contact=Contact(area=self.hood,policenumber="23456",policemail="police@email.com",hospitalnumber="465768",hospitalmail="hospital@email.com")
+    
+    def test_instance(self):
+        self.assertTrue(isinstance(self.contact,Contact))
+
+    def test_save_method(self):
+        self.contact.save_contact()
+        contacts=Contact.objects.all()
+        self.asertTrue(len(contacts)>0)
+
+    def test_delete_method(self):
+        self.contact.save_contact()
+        self.contact=Contact.objects.get(policenumber="23456")
+        self.contact.delete_contact()
+        contacts=Contact.objects.all()
+        self.assertTrue(len(contacts)==0)
 
 
 class ProfileTestCase(TestCase):
@@ -36,7 +58,9 @@ class ProfileTestCase(TestCase):
         User = get_user_model()
         self.daisy = User(username = "daisy", email="daisy@email.com", password = "password")
         self.daisy.save()
-        self.profile = Profile(bio="love life","daisy@email.com",user=self.daisy)
+        self.hood = Hood(name="local",location="area",occupants=5,adder=self.daisy)
+        self.hood.save()
+        self.profile = Profile(bio="love life",hood=self.hood,user=self.daisy)
 
     def test_instance(self):
         self.assertTrue(isinstance(self.profile, Profile))
@@ -48,13 +72,13 @@ class ProfileTestCase(TestCase):
 
     def test_update_method(self):
         self.profile.save_profile()
-        self.profile = Profile.objects.filter(contact = 'daisy@email.com').update(contact = 'maisy@email.com')
-        self.profile_update = Profile.objects.get(bio="love life")
-        self.assertTrue(self.profile_update.contact== 'maisy@email.com')
+        self.profile = Profile.objects.filter(bio = 'love life').update(bio = 'easy')
+        self.profile_update = Profile.objects.get(user=self.daisy)
+        self.assertTrue(self.profile_update.bio== 'easy')
 
     def test_delete_method(self):
         self.profile.save_profile()
-        self.profile=Profile.objects.get(bio='love life')
+        self.profile=Profile.objects.get(bio='easy')
         self.profile.delete_profile()
         profiles=Profile.objects.all()
         self.assertTrue(len(profiles)==0)
